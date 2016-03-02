b3b927fef1207af9ef1a1b350baac3b67a9131d8
Gundram Leifert <gundram.leifert@uni-rostock.de>
2016-03-02 10:30:35 +0100

    change image class, adding openCV instance, deleting byte[] instance

--- src/eu/transkribus/interfaces/Image.java
+++ src/eu/transkribus/interfaces/Image.java
@@ -8,7 +8,7 @@ package eu.transkribus.interfaces;
 import java.awt.Polygon;
 import java.awt.image.BufferedImage;
 import java.net.URL;
-import javax.imageio.ImageTypeSpecifier;
+import org.opencv.core.Mat;
 
 /**
  *
@@ -16,29 +16,29 @@ import javax.imageio.ImageTypeSpecifier;
  */
 public class Image {
 
-    private long imagePointer;
-    private byte[] imageData;
-    private int imageWidth;
-    private int imageHeight;
     private URL imageUrl;
     private BufferedImage imageBufferedImage;
+    private Mat imageOpenCVImage;
 
     private Polygon polygon;
     private String[] properties;
+    private Type type;
+
+    public enum Type {
+        URL, OPEN_CV, JAVA
+    }
 
     /**
-     * This type will also exist as instance in C++ <br/>
-     * Used for Java and C to refer to an image in the memory. In C++ and Java
-     * there have to be methods to load such an image
+     * This type already exists as instance in C++ <br/>
+     * In C++ and Java there have to be methods to use such an image. The
+     * position in the memory is accessible using
+     * this.imageOpenCVImage.nativeObject
      *
-     * @param imagePointer Pointer to the image in memory
-     * @param imageWidth
-     * @param imageHeight
+     * @param image
      */
-    public Image(long imagePointer, int imageWidth, int imageHeight) {
-        this.imagePointer = imagePointer;
-        this.imageHeight = imageHeight;
-        this.imageWidth = imageWidth;
+    public Image(Mat image) {
+        this.imageOpenCVImage = image;
+        type = Type.OPEN_CV;
     }
 
     /**
@@ -49,6 +49,7 @@ public class Image {
      */
     public Image(URL url) {
         this.imageUrl = url;
+        type = Type.URL;
     }
 
     /**
@@ -58,21 +59,7 @@ public class Image {
      */
     public Image(BufferedImage imageBufferedImage) {
         this.imageBufferedImage = imageBufferedImage;
-    }
-
-    /**
-     * when the image is neighter in local memory nor online, it has to be
-     * transfered directly. It has to be discussed, which format is the best and
-     * which image depth is enough (byte, float,...)
-     *
-     * @param imageData
-     * @param imageWidth
-     * @param imageHeight
-     */
-    public Image(byte[] imageData, int imageWidth, int imageHeight) {
-        this.imageData = imageData;
-        this.imageHeight = imageHeight;
-        this.imageWidth = imageWidth;
+        type = Type.JAVA;
     }
 
     public void setProperties(String[] properties) {
@@ -83,41 +70,82 @@ public class Image {
         this.polygon = polygon;
     }
 
-    public long getImagePointer() {
-        return imagePointer;
-    }
-
-    /**
-     * as first workaround - maybe deleted later
-     *
-     * @return
-     */
-    public BufferedImage getImageBufferedImage() {
-        return imageBufferedImage;
-    }
-
-    public byte[] getImageData() {
-        return imageData;
-    }
-
-    public URL getImageUrl() {
-        return imageUrl;
-    }
-
-    public int getImageWidth() {
-        return imageWidth;
-    }
-
-    public int getImageHeight() {
-        return imageHeight;
-    }
-
     public Polygon getPolygon() {
         return polygon;
     }
 
     public String[] getProperties() {
         return properties;
+    }
+
+    /**
+     * as first workaround
+     *
+     * @return
+     */
+    public Mat getImageOpenCVImage() {
+        if (imageOpenCVImage == null) {
+            throw new RuntimeException("no convertion done from " + type.toString() + " to " + Type.OPEN_CV + " implemented.");
+        }
+        return imageOpenCVImage;
+    }
+
+    /**
+     * as first workaround
+     *
+     * @return
+     */
+    public BufferedImage getImageBufferedImage() {
+        if (imageBufferedImage == null) {
+            throw new RuntimeException("no convertion done from " + type.toString() + " to " + Type.JAVA + " implemented.");
+        }
+        return imageBufferedImage;
+    }
+
+    public URL getImageUrl() {
+        if (type != Type.URL) {
+            throw new RuntimeException("no url given, image was set with type " + type.toString() + ".");
+        }
+        return imageUrl;
+    }
+
+    /**
+     * shows if a given type is supported
+     *
+     * @param type
+     * @return
+     */
+    public boolean hasType(Type type) {
+        switch (type) {
+            case JAVA:
+                return imageBufferedImage != null;
+            case OPEN_CV:
+                return imageOpenCVImage != null;
+            case URL:
+                return imageUrl != null;
+            default:
+                throw new RuntimeException("unknown type '" + type + "'");
+        }
+    }
+
+    /**
+     * create internal repesentation for the given type, if possible.
+     *
+     * @param toType
+     */
+    public void convert(Type toType) {
+        switch (toType) {
+            case JAVA:
+//                imageBufferedImage=... create instance in Java;
+                break;
+            case OPEN_CV:
+//                imageOpenCVImage=... create instance in C++;
+                break;
+            case URL:
+                throw new RuntimeException("cannot convert from instance to url");
+            default:
+                throw new RuntimeException("unknown type '" + toType + "'");
+        }
     }
 
 }
