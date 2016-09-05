21a74d2c8e35166fb09b968cead46559428da0be
Gundram Leifert <gundram.leifert@uni-rostock.de>
2016-09-05 14:30:24 +0200

    add description of IText2Image interface

--- src/main/java/eu/transkribus/interfaces/IText2Image.java
+++ src/main/java/eu/transkribus/interfaces/IText2Image.java
@@ -10,33 +10,49 @@ import java.io.File;
 import eu.transkribus.interfaces.types.Image;
 
 /**
- * For a given input, the representation (Confmat, WordGraph,...) is calculated.
- * Probably this interface is too simple and have to be changed.
+ * This tool can be used to match a (ASCII-)transcription to given
+ * baselines.<br\>
+ * INPUT:<br/>
+ * Text-file: The UTF-8-coded text-file (diplomatic text) where U+000A ('LINE
+ * FEED (LF)') denotes a new line.<br/>
+ * PageXML-file: The PageXML-file have to contain baselines with its
+ * corresponding surrounding polygon.<br/>
+ * Image: The image corresponding to the PageXML-file.<br/>
+ * Models: pathes to Optical and Language Model.<br/>
+ * CharMap: In many cases the models cannot handle all characters from the
+ * transcription text. The CharMap can be used to allocate specific character to
+ * existing channels.<br/>
+ * Properties: When the algorithm has some Properties/Thresholds, this structure
+ * can be used to set them.
+ *
  *
  * @author gundram
  */
-public interface IText2Image extends IModule{
-	
+public interface IText2Image extends IModule {
+
     /**
      * in URO-CITlab this is done by an MDRNN (sprnn). Output in this case would
      * be a Confidence-Matrix.
      *
-     * @param pathToOpticalModel is either path to serialized RNN or path to GMM/HMM.
-     * @param pathToLanguageModel is either path to ARPA-file or other language resource file.
-     * @param pathToCharacterMap is path to character map file (TODO: add link to file description)
-     * @param pathToText is path to an UTF-8 coded text file 
+     * @param pathToOpticalModel is either path to serialized RNN or path to
+     * GMM/HMM.
+     * @param pathToLanguageModel is either path to ARPA-file or other language
+     * resource file.
+     * @param pathToCharacterMap is path to character map file (TODO: add link
+     * to file description)
+     * @param pathToText is path to an UTF-8 coded text file
      * @param image
-     * @param xmlInOut 
+     * @param xmlInOut
      * @param props set of parameters for recognition (has to be documented)
      */
     public void match(
-        String pathToOpticalModel,
-        String pathToLanguageModel,
-        String pathToCharacterMap,
-        String pathToText,
-        Image image, 
-        String xmlInOut, 
-        String[] props 
-        );
-    
+            String pathToOpticalModel,
+            String pathToLanguageModel,
+            String pathToCharacterMap,
+            String pathToText,
+            Image image,
+            String xmlInOut,
+            String[] props
+    );
+
 }
