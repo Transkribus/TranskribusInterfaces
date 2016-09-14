242aa955a6f8504b8e6a0999d8cefc9c221cee58
Mauricio Villegas <mauricio_ville@yahoo.com>
2016-09-14 12:16:01 +0200

    Some changes to the IText2Image interface

--- src/main/java/eu/transkribus/interfaces/IText2Image.java
+++ src/main/java/eu/transkribus/interfaces/IText2Image.java
@@ -10,8 +10,7 @@ import java.io.File;
 import eu.transkribus.interfaces.types.Image;
 
 /**
- * This tool can be used to match a (ASCII-)transcription to given baselines and
- * words.<br\>
+ * This tool can be used to match a given transcript to a page (image+baselines).<br\>
  * INPUT:<br/>
  * Text-file(s): The UTF-8-coded text-file(s) (diplomatic text) where U+000A
  * ('LINE FEED (LF)') denotes a new line.<br/>
@@ -19,15 +18,16 @@ import eu.transkribus.interfaces.types.Image;
  * corresponding surrounding polygon.<br/>
  * Image: The image corresponding to the PageXML-file or a List of pathes to
  * Images<br/>
- * Models: pathes to Optical and Language Model. These Models are optional<br/>
+ * Models: pathes to Optical and Language Model. These Models may be optional
+ * depending on the implementation.<br/>
  * CharMap: In many cases the models cannot handle all characters from the
  * transcription text. The CharMap can be used to allocate specific character to
- * existing channels. The CharMap is optional<br/>
+ * existing channels. The CharMap is optional.<br/>
  * Properties: When the algorithm has some Properties/Thresholds, this structure
  * can be used to set them. If props has the property "words:=true", an
  * alignment on word-level is done.<br/>
  * Region IDs: If the algorithm should only work on specific regions, the
- * region_id of the PageXML file can be given.
+ * regionIds of the PageXML file can be given.
  * <br/>
  * If a match is found, the algorithm tries to calculate a confidence of this
  * match. This confidence is saved in the TextEquiv-tag as "conf".
@@ -39,15 +39,16 @@ public interface IText2Image extends IModule {
 
     /**
      * The methods tries to align a given text to a whole Collection. It is
-     * assumed that the text (pathToText) has no page breaks, Line breaks are
-     * optional
+     * assumed that the text (pathToText) has no page breaks. Line breaks may
+     * be optional, depending on the implementation. The length of pathsToImages
+     * and pathsToXmlInOut has to be the same.
      *
      * @param pathToOpticalModel
      * @param pathToLanguageModel
      * @param pathToCharacterMap
      * @param pathToText
-     * @param images has to be from same size as xmlInOut
-     * @param xmlInOut has to be from same size as images
+     * @param pathsToImages
+     * @param pathsToXmlInOut
      * @param props
      */
     public void matchCollection(
@@ -55,8 +56,8 @@ public interface IText2Image extends IModule {
             String pathToLanguageModel,
             String pathToCharacterMap,
             String pathToText,
-            String[] images,
-            String[] xmlInOut,
+            String[] pathsToImages,
+            String[] pathsToXmlInOut,
             String[] props
     );
 
@@ -68,8 +69,8 @@ public interface IText2Image extends IModule {
      * @param pathToLanguageModel
      * @param pathToCharacterMap
      * @param pathToText
-     * @param images
-     * @param xmlInOut
+     * @param image
+     * @param pathToXmlInOut
      * @param regionIds
      * @param props
      */
@@ -78,22 +79,22 @@ public interface IText2Image extends IModule {
             String pathToLanguageModel,
             String pathToCharacterMap,
             String pathToText,
-            Image images,
-            String xmlInOut,
+            Image image,
+            String pathToXmlInOut,
             String[] regionIds,
             String[] props
     );
 
     /**
      * This method tries to match a set of transcriptions to a set of regions.
-     * The length of pathToText has to fit the length of regionIds.
+     * The length of pathsToText has to fit the length of regionIds.
      *
      * @param pathToOpticalModel
      * @param pathToLanguageModel
      * @param pathToCharacterMap
-     * @param pathToText
-     * @param images
-     * @param xmlInOut
+     * @param pathsToText
+     * @param image
+     * @param pathToXmlInOut
      * @param regionIds
      * @param props
      */
@@ -101,9 +102,9 @@ public interface IText2Image extends IModule {
             String pathToOpticalModel,
             String pathToLanguageModel,
             String pathToCharacterMap,
-            String[] pathToText,
-            String images,
-            String xmlInOut,
+            String[] pathsToText,
+            String image,
+            String pathToXmlInOut,
             String[] regionIds,
             String[] props
     );
