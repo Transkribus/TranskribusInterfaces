cbe54b6988d23ec0324df254bad01032744c0385
kahlep <philip.kahle@uibk.ac.at>
2016-11-17 14:20:30 +0100

    added dictionary and entry interfaces

---
+++ src/main/java/eu/transkribus/interfaces/IEntry.java
@@ -0,0 +1,17 @@
+package eu.transkribus.interfaces;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author jnphilipp
+ */
+public interface IEntry {
+    public String getKey();
+    public int getFrequency();
+    public int getFrequency(String name) throws NoSuchElementException;
+    public Map<String, Integer> getValues();
+    public boolean containsValue(String name);
+}
