07a64ad2d5980aed73c40a0faca3c36ff8d0e998
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-04-07 14:36:44 +0200

    add KSW interface

--- src/main/java/eu/transkribus/interfaces/IKeywordSpotter.java
+++ src/main/java/eu/transkribus/interfaces/IKeywordSpotter.java
@@ -15,29 +15,13 @@ public interface IKeywordSpotter extends IModule {
 
     /**
      *
-     * @param pathToOpticalModel path to optical model (always needed)
-     * @param pathToLanguageModel path to language model or language resource
-     * (optional)
-     * @param pathToCharacterMap path to charmap (optional, only needed if
-     * keywords/LM differs from OM)
-     * @param image image where search should be done (can be null)
-     * @param xmlIn xml-file to of image (always needed)
-     * @param keywords unicode-coded file with keywords in each line
-     * @param storageDir directory to already cached structure (like wordgraphs,
-     * confmats)
-     * @param result path where the json-file with results should be saved
-     * @param props additional parameters
+     * @param folderIn folder which contains storages
+     * @param queriesIn file which contains query strings
+     * @param dictIn file which contains a unigram of possible words (can be
+     * null)
+     * @param resultOut path to json-output file
+     * @param props properties (can be null)
      */
-    public void process(
-            String pathToOpticalModel,
-            String pathToLanguageModel,
-            String pathToCharacterMap,
-            Image image,
-            Image xmlIn,
-            String keywords,
-            String storageDir,
-            String result,
-            String[] props
-    );
+    public void process(String folderIn, String queriesIn, String dictIn, String resultOut, String[] props);
 
 }
