2dfa6f00cbf09c577a6cd11d8bab37282f12b1b0
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-05-18 16:29:35 +0200

    add HTR-method which uses "abbreviation dictionary"

--- src/main/java/eu/transkribus/interfaces/IHtr.java
+++ src/main/java/eu/transkribus/interfaces/IHtr.java
@@ -5,8 +5,6 @@
  */
 package eu.transkribus.interfaces;
 
-import java.io.File;
-
 import eu.transkribus.interfaces.types.Image;
 
 /**
@@ -15,43 +13,70 @@ import eu.transkribus.interfaces.types.Image;
  *
  * @author gundram
  */
-public interface IHtr extends IModule{
-	
+public interface IHtr extends IModule {
+
 //	public void createModel(String path, String[] pars);
+    /**
+     * in URO-CITlab this is done by an MDRNN (sprnn). Output in this case would
+     * be a Confidence-Matrix.
+     *
+     * @param pathToOpticalModel is either path to serialized RNN or path to
+     * GMM/HMM.
+     * @param pathToLanguageModel is either path to ARPA-file or other language
+     * resource file.
+     * @param pathToCharacterMap is path to character map file (TODO: add link
+     * to file description)
+     * @param image
+     * @param xmlInOut
+     * @param storageDir
+     * @param lineIds
+     * @param props set of parameters for recognition (has to be documented)
+     */
+    public void process(
+            String pathToOpticalModel,
+            String pathToLanguageModel,
+            String pathToCharacterMap,
+            Image image,
+            String xmlInOut,
+            String storageDir,
+            String[] lineIds,
+            String[] props
+    );
 
     /**
      * in URO-CITlab this is done by an MDRNN (sprnn). Output in this case would
      * be a Confidence-Matrix.
      *
-     * @param pathToOpticalModel is either path to serialized RNN or path to GMM/HMM.
-     * @param pathToLanguageModel is either path to ARPA-file or other language resource file.
-     * @param pathToCharacterMap is path to character map file (TODO: add link to file description)
+     * @param pathToOpticalModel is either path to serialized RNN or path to
+     * GMM/HMM.
+     * @param pathToLanguageModel is either path to ARPA-file or other language
+     * resource file.
+     * @param pathToCharacterMap is path to character map file (TODO: add link
+     * to file description)
      * @param image
      * @param xmlInOut
      * @param storageDir
-     * @param lineIds 
+     * @param pathToAbbrDict
+     * @param lineIds
      * @param props set of parameters for recognition (has to be documented)
      */
     public void process(
-        String pathToOpticalModel,
-        String pathToLanguageModel,
-        String pathToCharacterMap,
-        Image image, 
-        String xmlInOut, 
-        String storageDir, 
-        String[] lineIds,
-        String[] props 
-        );
-    
+            String pathToOpticalModel,
+            String pathToLanguageModel,
+            String pathToCharacterMap,
+            Image image,
+            String xmlInOut,
+            String storageDir,
+            String pathToAbbrDict,
+            String[] lineIds,
+            String[] props
+    );
+
 //    public static boolean process(String pathToModels, Image image, String pageXmlIn, String pageXmlOut, String storageDir, String[] props);
 //    public boolean process(Image image, String pageXmlIn, String pageXmlOut, String storageDir, String[] props);
 //    public boolean processDecoding(String pathToModel, Image image, String pageXmlIn, String pageXmlOut, String storageDir, String[] props);
-
 //    public boolean processBoth(String pathToModel, Image image, String pageXmlIn, String pageXmlOut, String storageDir, String[] props);
     //public boolean processOutput(Serializable representation, String[] properties);
-
 //    public String processOutput(File path, String[] properties);
-
 //    public Serializable processRepresentation(Image pageImage, Region[] lineRegions, String pathToModel, String[] properties);
-    
 }
