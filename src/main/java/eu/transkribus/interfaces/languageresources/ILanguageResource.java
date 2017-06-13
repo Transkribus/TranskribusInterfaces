1db4ea0518b8b352d58e995dc70309760c0bf497
Maximilian Bryan <bryan@informatik.uni-leipzig.de>
2017-06-13 11:08:32 +0200

    added interfaces for language resources

---
+++ src/main/java/eu/transkribus/interfaces/languageresources/ILanguageResource.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.transkribus.interfaces.languageresources;
+
+import eu.transkribus.interfaces.IDictionary;
+import eu.transkribus.interfaces.ITokenizer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author max
+ */
+public interface ILanguageResource
+{
+    IDictionary getFrequencyDictionary(ITokenizer tokenizer, String text);
+}
