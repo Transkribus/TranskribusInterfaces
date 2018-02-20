9403307f2f5568b45053cfd96ad15a40820edb53
kahlep <philip.kahle@uibk.ac.at>
2018-02-20 12:12:33 +0100

    set java library path in @Before

--- src/test/java/eu/transkribus/interfaces/native_wrapper/NativeProxyUtilsTest.java
+++ src/test/java/eu/transkribus/interfaces/native_wrapper/NativeProxyUtilsTest.java
@@ -5,12 +5,21 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import eu.transkribus.interfaces.types.Image;
+import eu.transkribus.interfaces.types.util.SysPathUtils;
 
 public class NativeProxyUtilsTest {
 
+	@Before
+	public void setupEnvironment() {
+		SysPathUtils.addDirToPath("/usr/local/lib");
+		SysPathUtils.addDirToPath("/usr/local/share/OpenCV/java");
+		Image.registerImageIOServices();
+	}
+	
 	@Test
 	public void testToNativeImage() throws IOException {
 		File imgFile = new File("src/test/resources/img/035_320_001.jpg");
