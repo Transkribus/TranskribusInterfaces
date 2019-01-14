e3da2b1155f829b7378611a964826e2d827d0c86
kahlep <philip.kahle@uibk.ac.at>
2019-01-14 18:07:56 +0100

    TrpImgMdParser: check all EXIF directories for required tags

--- src/main/java/eu/transkribus/interfaces/types/util/TrpImgMdParser.java
+++ src/main/java/eu/transkribus/interfaces/types/util/TrpImgMdParser.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Collection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,7 @@ import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.MetadataException;
 import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifDirectoryBase;
 import com.drew.metadata.exif.ExifIFD0Directory;
 
 import eu.transkribus.interfaces.util.URLUtils;
@@ -73,6 +75,9 @@ public class TrpImgMdParser {
 	 * Inspect the image metadata for width, height and EXIF orientation tag value.
 	 * The returned {@link ImageTransformation} object includes this data as well as the width and height of the image
 	 * after rotating it according to the orienation tag value and an {@link AffineTransform} for doing so.
+	 * <br><br>
+	 * This method searches specific EXIF directories only!! This might fail as e.g. Windows Photo Editor seems to push around tags into sub-dirs.
+	 * See doc ID=5869 on test server. 
 	 * 
 	 * @param url
 	 * @return
@@ -84,50 +89,59 @@ public class TrpImgMdParser {
 			throws ImageProcessingException, IOException, MetadataException {
 		long time = System.currentTimeMillis();
 		Metadata metadata = ImageMetadataReader.readMetadata(is);
-		ExifIFD0Directory exifIFD0Directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+		
+		
+		//tags should be in ExifIFD0Directory or ExifSubIFDDirectory objects
+		//Windows Photo Editor 10.x seems to push tags into a sub-dir on edit. See doc ID=5869 on test server
+		Collection<ExifDirectoryBase> dirs = metadata.getDirectoriesOfType(ExifDirectoryBase.class);
+		
 		if(logger.isTraceEnabled()) {
 			for (Directory directory : metadata.getDirectories()) {
-				logger.trace("Directory = " + directory.getName());
+				logger.debug("Directory = " + directory.getName());
 			    for (Tag tag : directory.getTags()) {
-			    	logger.trace("" + tag);
+			    	logger.debug("" + tag);
 			    }
 			}
 		}
-		if(exifIFD0Directory == null) {
+		
+		if(dirs.isEmpty()) {
 			throw new MetadataException("No EXIF data found.");
 		} else {
-			int width = getExifTagValueInt(exifIFD0Directory, 0, ExifIFD0Directory.TAG_IMAGE_WIDTH, 
+			int width = getExifTagValueInt(dirs, 0, ExifIFD0Directory.TAG_IMAGE_WIDTH, 
 					ExifIFD0Directory.TAG_EXIF_IMAGE_WIDTH);
-			int height = getExifTagValueInt(exifIFD0Directory, 0, ExifIFD0Directory.TAG_IMAGE_HEIGHT, 
+			int height = getExifTagValueInt(dirs, 0, ExifIFD0Directory.TAG_IMAGE_HEIGHT, 
 					ExifIFD0Directory.TAG_EXIF_IMAGE_HEIGHT);
-			final int orientation = getExifTagValueInt(exifIFD0Directory, DEFAULT_EXIF_ORIENTATION, 
+			final int orientation = getExifTagValueInt(dirs, DEFAULT_EXIF_ORIENTATION, 
 					ExifIFD0Directory.TAG_ORIENTATION);
 			logger.debug("Exif orientation read from stream in " + (System.currentTimeMillis() - time) + " ms");
 			
 			return getTransformation(new ImageDimension(orientation, width, height));
 		}
 	}
-
+	
 	/**
-	 * Extract an int value from the exif directory.
+	 * Extract an int value from the exif directories.
 	 * Accepts a list of tags to be searched for a value in the given order. First value found is returned.
 	 * If none is found, defaultValue is returned.
 	 * 
-	 * @param dir
+	 * @param dirs the exif directories to search in given order
 	 * @param defaultValue returned if nothing is found
 	 * @param tags a list of tags to be inspected in this order.
 	 * @return
 	 */
-	private static int getExifTagValueInt(ExifIFD0Directory dir, int defaultValue, final int... tags) {
-		for(int tag : tags) {
-			if(dir.containsTag(tag)) {
-				try {
-					return dir.getInt(tag);
-				} catch (MetadataException e) {
-					logger.error("Could not extract Exif data value for tag: " + tag, e);
+	private static int getExifTagValueInt(Collection<ExifDirectoryBase> dirs, int defaultValue, final int... tags) {
+		for(ExifDirectoryBase dir : dirs) {
+			for(int tag : tags) {
+				if(dir.containsTag(tag)) {
+					try {
+						logger.debug("Found value in Exif directory '" + dir.getName() + "' for tag: " + tag + " = " + dir.getInt(tag));
+						return dir.getInt(tag);
+					} catch (MetadataException e) {
+						logger.error("Could not extract Exif data value from directory '" + dir.getName() + "' for tag: " + tag, e);
+					}
+				} else {
+					logger.debug("No value set in Exif directory '" + dir.getName() + "' for tag: " + tag);
 				}
-			} else {
-				logger.debug("No value set in Exif data for tag: " + tag);
 			}
 		}
 		return defaultValue;
